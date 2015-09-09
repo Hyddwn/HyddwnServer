554a9a3ee34ea6012bdfbbe79cd3f07f6066f4b8
exec <exec@mabinoger.de>
2015-09-09 12:22:02 +0200

    Fixed crash in channel on closing login server
    Since the servers communicate with each other just like with the
    players, the clean up is called when closing the login server, since the
    channel sees the connection as any other. But a login server doesn't
    have a controlled creature, which resulted in a null ref.

--- src/ChannelServer/Network/ChannelClient.cs
+++ src/ChannelServer/Network/ChannelClient.cs
@@ -104,9 +104,9 @@ namespace Aura.Channel.Network
 		/// </summary>
 		public override void CleanUp()
 		{
-
-			// Moved here to always be called when a client is being killed off, on the suggestion of Exec.
-			ChannelServer.Instance.Events.OnPlayerDisconnect(Controlling);
+			// Moved here to always be called when a client is being killed off.
+			if (this.Controlling != null)
+				ChannelServer.Instance.Events.OnPlayerDisconnect(this.Controlling);
 
 			// Dispose creatures, to remove subscriptions and stuff.
 			// Do this before unspawning, the creature might need the region.
