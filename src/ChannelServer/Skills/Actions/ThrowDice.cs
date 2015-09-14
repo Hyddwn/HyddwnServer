9ae5418bd422bc570ad70d631d38d0ddcdfa7918
Unknownvoid <Iceygrass@gmail.com>
2015-09-14 14:29:22 -0700

    Code Fixer Results.

--- src/ChannelServer/Skills/Actions/ThrowDice.cs
+++ src/ChannelServer/Skills/Actions/ThrowDice.cs
@@ -30,33 +30,33 @@ namespace Aura.Channel.Skills.Action
 	/// </remarks>
 	///
 
-    [Skill(SkillId.DiceTossing)]
+	[Skill(SkillId.DiceTossing)]
 	public class DiceTossing : ISkillHandler, IPreparable, IReadyable, IUseable, ICompletable, ICancelable
 	{
-        private const int ItemId = 62021;
+		private const int ItemId = 62021;
 
-        /// <summary>
-        /// Prepares skill, fails if no Dice is found.
-        /// </summary>
-        /// <param name="creature"></param>
-        /// <param name="skill"></param>
-        /// <param name="packet"></param>
-        /// <returns></returns>
-        public bool Prepare(Creature creature, Skill skill, Packet packet)
+		/// <summary>
+		/// Prepares skill, fails if no Dice is found.
+		/// </summary>
+		/// <param name="creature"></param>
+		/// <param name="skill"></param>
+		/// <param name="packet"></param>
+		/// <returns></returns>
+		public bool Prepare(Creature creature, Skill skill, Packet packet)
 		{
-            if (creature.Inventory.Count(ItemId) == 0)
-            {
-                Send.Notice(creature, Localization.Get("You need at least one Six Sided Dice."));
-                return false;
-            }
+			if (creature.Inventory.Count(ItemId) == 0)
+			{
+				Send.Notice(creature, Localization.Get("You need at least one Six Sided Dice."));
+				return false;
+			}
 
-            creature.StopMove();
+			creature.StopMove();
 
-            Send.UseMotion(creature, 27, 0, false, false);
-            Send.Effect(creature, Effect.Dice, "prepare");
-            Send.SkillPrepare(creature, skill.Info.Id, skill.GetCastTime());
+			Send.UseMotion(creature, 27, 0, false, false);
+			Send.Effect(creature, Effect.Dice, "prepare");
+			Send.SkillPrepare(creature, skill.Info.Id, skill.GetCastTime());
 
-            return true;
+			return true;
 		}
 
 		/// <summary>
@@ -68,12 +68,12 @@ namespace Aura.Channel.Skills.Action
 		/// <returns></returns>
 		public bool Ready(Creature creature, Skill skill, Packet packet)
 		{
-            Send.SkillStackSet(creature, skill.Info.Id, 1);
-            Send.UseMotion(creature, 27, 1, true, false);
-            Send.Effect(creature, Effect.Dice, "wait");
-            Send.SkillReady(creature, skill.Info.Id);
-            
-            return true;
+			Send.SkillStackSet(creature, skill.Info.Id, 1);
+			Send.UseMotion(creature, 27, 1, true, false);
+			Send.Effect(creature, Effect.Dice, "wait");
+			Send.SkillReady(creature, skill.Info.Id);
+
+			return true;
 		}
 
 		/// <summary>
@@ -84,17 +84,17 @@ namespace Aura.Channel.Skills.Action
 		/// <param name="packet"></param>
 		public void Use(Creature creature, Skill skill, Packet packet)
 		{
-            var unklong = packet.GetLong();
-            var unkInt3 = packet.GetInt();
-            var unkInt4 = packet.GetInt();
+			var unklong = packet.GetLong();
+			var unkInt3 = packet.GetInt();
+			var unkInt4 = packet.GetInt();
 
-           // Send.ItemAmount(creature, ); Remove 1 item
-            Send.UseMotion(creature, 27, 2, false, false);
-            Send.Effect(creature, Effect.Dice, "process", unklong, (byte)3);
-            Send.SkillUse(creature, skill.Info.Id, unklong, unkInt3, unkInt4);
-            skill.Stacks = 0;
-            Send.Effect(creature, Effect.StackUpdate, Effect.Dice, (byte)0, (byte)1, (byte)0);
-        }
+			// Send.ItemAmount(creature, ); Remove 1 item
+			Send.UseMotion(creature, 27, 2, false, false);
+			Send.Effect(creature, Effect.Dice, "process", unklong, (byte)3);
+			Send.SkillUse(creature, skill.Info.Id, unklong, unkInt3, unkInt4);
+			skill.Stacks = 0;
+			Send.Effect(creature, Effect.StackUpdate, Effect.Dice, (byte)0, (byte)1, (byte)0);
+		}
 
 		/// <summary>
 		/// Completes skill
@@ -104,9 +104,9 @@ namespace Aura.Channel.Skills.Action
 		/// <param name="packet"></param>
 		public void Complete(Creature creature, Skill skill, Packet packet)
 		{
-            var unklong = packet.GetLong();
-            var unkInt3 = packet.GetInt();
-            var unkInt4 = packet.GetInt();
+			var unklong = packet.GetLong();
+			var unkInt3 = packet.GetInt();
+			var unkInt4 = packet.GetInt();
 
 			Send.SkillComplete(creature, skill.Info.Id, unklong, unkInt3, unkInt4);
 		}
@@ -118,11 +118,11 @@ namespace Aura.Channel.Skills.Action
 		/// <param name="skill"></param>
 		public void Cancel(Creature creature, Skill skill)
 		{
-            skill.Stacks = 0;
-            Send.Effect(creature, Effect.StackUpdate, Effect.Dice, (byte)0, (byte)1);
-            Send.MotionCancel2(creature, 1);
-            Send.Effect(creature, Effect.Dice, "cancel");
-            Send.SkillCancel(creature);
-        }
+			skill.Stacks = 0;
+			Send.Effect(creature, Effect.StackUpdate, Effect.Dice, (byte)0, (byte)1);
+			Send.MotionCancel2(creature, 1);
+			Send.Effect(creature, Effect.Dice, "cancel");
+			Send.SkillCancel(creature);
+		}
 	}
 }
