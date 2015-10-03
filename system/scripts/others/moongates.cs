437bbb169939681f16d877cf8809beac2e74289f
exec <exec@mabinoger.de>
2015-10-03 11:28:17 +0200

    Added moon gate titles and confirmation
    It's not perfect, pets won't see the title and confirmation unless
    MoonTunnel is disabled, but Aura's system doesn't really allow creature
    specific packet structures.

--- system/scripts/others/moongates.cs
+++ system/scripts/others/moongates.cs
@@ -13,63 +13,63 @@ public class MoongateScript : GeneralScript
 	// Setup ----------------------------------------------------------------
 
 	// Moongates are always open
-	private const bool AlwaysOpen = true;
+	private const bool AlwaysOpen = false;
 
 	// Anybody can use any moongate without activating it first
 	private const bool FreeRoaming = false;
 
 	public override void Load()
 	{
-		RegisterGate("_moontunnel_tirchonaill", 0xA000010004001F);
-		RegisterGate("_moontunnel_ciar_dungeon", 0xA000010005001B);
-		RegisterGate("_moontunnel_alby_dungeon", 0xA0000100080006);
-		RegisterGate("_moontunnel_math_dungeon", 0xA0000E00090034);
-		RegisterGate("_moontunnel_dunbarton", 0xA0000E000A02B4);
-		RegisterGate("_moontunnel_rabbie_dungeon", 0xA0000E00110039);
-		RegisterGate("_moontunnel_duntir02", 0xA000100008002B);
-		RegisterGate("_moontunnel_loggingcamp", 0xA00010000D0011);
-		RegisterGate("_moontunnel_cobh_harbor", 0xA0001700040199);
-		RegisterGate("_moontunnel_reighinalt", 0xA0001E00010062);
-		RegisterGate("_moontunnel_dragonruin", 0xA0001E00020064);
-		RegisterGate("_moontunnel_fiodh_dungeon", 0xA0001E00050002);
-		RegisterGate("_moontunnel_bangor", 0xA0001E0008002D);
-		RegisterGate("_moontunnel_sidhe_sneachta", 0xA0003000020003);
-		RegisterGate("_moontunnel_emainmacha", 0xA000340008003D);
-		RegisterGate("_moontunnel_emain_macha_w", 0xA00034000F001D);
-		RegisterGate("_moontunnel_coill_dungeon", 0xA0003400150099);
-		RegisterGate("_moontunnel_emain_macha_n", 0xA000340016001E);
-		RegisterGate("_moontunnel_sen_mag_plains", 0xA0003500000007);
-		RegisterGate("_moontunnel_peaca_dungeon", 0xA0003500070004);
-		RegisterGate("_moontunnel_ceoisland", 0xA0003800010001);
-		RegisterGate("_moontunnel_morva_aisle", 0xA0006000010003);
-		RegisterGate("_moontunnel_ceann_harbor", 0xA0006400000006);
-		RegisterGate("_moontunnel_tailteann_w", 0xA0012C000A0001);
-		RegisterGate("_moontunnel_tailteann_farm01", 0xA0012C00170082);
-		RegisterGate("_moontunnel_tailteann_druid", 0xA0012C0019002E);
-		RegisterGate("_moontunnel_taillteann_west", 0xA0012C001900C3);
-		RegisterGate("_moontunnel_tailteann_s", 0xA0012C00240012);
-		RegisterGate("_moontunnel_tailteann_e", 0xA0012C00260016);
-		RegisterGate("_moontunnel_tailteann_cemetery", 0xA0012C002F0001);
-		RegisterGate("_moontunnel_sliab_cuilin01", 0xA0012D00040005);
-		RegisterGate("_moontunnel_sliab_cuilin02", 0xA0012D0004001B);
-		RegisterGate("_moontunnel_abb_neagh01", 0xA0012E00190018);
-		RegisterGate("_moontunnel_abb_neagh02", 0xA0012E001A0016);
-		RegisterGate("_moontunnel_tara_01", 0xA001910008000A);
-		RegisterGate("_moontunnel_tara_west", 0xA00191000802F2);
-		RegisterGate("_moontunnel_tara_s", 0xA001910010002F);
-		RegisterGate("_moontunnel_blago_prairie_e", 0xA001920000003B);
-		RegisterGate("_moontunnel_blago_prairie_02", 0xA0019200020108);
-		RegisterGate("_moontunnel_blago_prairie_01", 0xA001920006002C);
-		RegisterGate("_moontunnel_tara_kingdom", 0xA001B5000E0003);
-		RegisterGate("_moontunnel_belfast_01", 0xA00FA500020024);
-		RegisterGate("_moontunnel_belfast_02", 0xA00FA5000B0011);
-		RegisterGate("_moontunnel_scathach_03", 0xA00FAE00020004);
-		RegisterGate("_moontunnel_scathach_04", 0xA00FAE00030005);
-		RegisterGate("_moontunnel_scathach_01", 0xA00FAE000A00B5);
-		RegisterGate("_moontunnel_scathach_02", 0xA00FAE000A00B8);
-		RegisterGate("_moontunnel_scathach_05", 0xA00FAE000C004B);
+		RegisterGate("Tir Chonaill", "_moontunnel_tirchonaill", 0xA000010004001F);
+		RegisterGate("Ciar Dungeon", "_moontunnel_ciar_dungeon", 0xA000010005001B);
+		RegisterGate("Aly Dungeon", "_moontunnel_alby_dungeon", 0xA0000100080006);
+		RegisterGate("Math Dungeon", "_moontunnel_math_dungeon", 0xA0000E00090034);
+		RegisterGate("Dunbarton", "_moontunnel_dunbarton", 0xA0000E000A02B4);
+		RegisterGate("Rabbie Dungeon", "_moontunnel_rabbie_dungeon", 0xA0000E00110039);
+		RegisterGate("Spiral Hill", "_moontunnel_duntir02", 0xA000100008002B);
+		RegisterGate("Logging Gamp", "_moontunnel_loggingcamp", 0xA00010000D0011);
+		RegisterGate("Lighthouse", "_moontunnel_cobh_harbor", 0xA0001700040199);
+		RegisterGate("Reighinalt", "_moontunnel_reighinalt", 0xA0001E00010062);
+		RegisterGate("Dragon Ruins", "_moontunnel_dragonruin", 0xA0001E00020064);
+		RegisterGate("Fiodh Dungeon", "_moontunnel_fiodh_dungeon", 0xA0001E00050002);
+		RegisterGate("Bangor", "_moontunnel_bangor", 0xA0001E0008002D);
+		RegisterGate("Sidhe Sneachta", "_moontunnel_sidhe_sneachta", 0xA0003000020003);
+		RegisterGate("Emain Macha", "_moontunnel_emainmacha", 0xA000340008003D);
+		RegisterGate("Emain Macha Western", "_moontunnel_emain_macha_w", 0xA00034000F001D);
+		RegisterGate("Coill Dungeon", "_moontunnel_coill_dungeon", 0xA0003400150099);
+		RegisterGate("Emain Macha Northern", "_moontunnel_emain_macha_n", 0xA000340016001E);
+		RegisterGate("Sen Mag Plains", "_moontunnel_sen_mag_plains", 0xA0003500000007);
+		RegisterGate("Peaca Dungeon", "_moontunnel_peaca_dungeon", 0xA0003500070004);
+		RegisterGate("Ceo Island", "_moontunnel_ceoisland", 0xA0003800010001);
+		RegisterGate("Morva Aisle", "_moontunnel_morva_aisle", 0xA0006000010003);
+		RegisterGate("Ceann Harbor", "_moontunnel_ceann_harbor", 0xA0006400000006);
+		RegisterGate("Tailteann", "_moontunnel_tailteann_w", 0xA0012C000A0001);
+		RegisterGate("Tailteann Farm", "_moontunnel_tailteann_farm01", 0xA0012C00170082);
+		RegisterGate("Druid's House", "_moontunnel_tailteann_druid", 0xA0012C0019002E);
+		RegisterGate("Tailteann West", "_moontunnel_taillteann_west", 0xA0012C001900C3);
+		RegisterGate("Tailteann South", "_moontunnel_tailteann_s", 0xA0012C00240012);
+		RegisterGate("Tailteann Eastern", "_moontunnel_tailteann_e", 0xA0012C00260016);
+		RegisterGate("Graveyard", "_moontunnel_tailteann_cemetery", 0xA0012C002F0001);
+		RegisterGate("Sliab Cuilin 1", "_moontunnel_sliab_cuilin01", 0xA0012D00040005);
+		RegisterGate("Sliab Cuilin 2", "_moontunnel_sliab_cuilin02", 0xA0012D0004001B);
+		RegisterGate("Abb Neagh 1", "_moontunnel_abb_neagh01", 0xA0012E00190018);
+		RegisterGate("Abb Neagh 2", "_moontunnel_abb_neagh02", 0xA0012E001A0016);
+		RegisterGate("Jousting Arena", "_moontunnel_tara_01", 0xA001910008000A);
+		RegisterGate("Tara", "_moontunnel_tara_west", 0xA00191000802F2);
+		RegisterGate("Tara South", "_moontunnel_tara_s", 0xA001910010002F);
+		RegisterGate("East Blago Prairie", "_moontunnel_blago_prairie_e", 0xA001920000003B);
+		RegisterGate("Eluned Winery", "_moontunnel_blago_prairie_02", 0xA0019200020108);
+		RegisterGate("Lezarro Winery", "_moontunnel_blago_prairie_01", 0xA001920006002C);
+		RegisterGate("Rath Royal Castle", "_moontunnel_tara_kingdom", 0xA001B5000E0003);
+		RegisterGate("Belfast 1", "_moontunnel_belfast_01", 0xA00FA500020024);
+		RegisterGate("Belfast 2", "_moontunnel_belfast_02", 0xA00FA5000B0011);
+		RegisterGate("Scathach 3", "_moontunnel_scathach_03", 0xA00FAE00020004);
+		RegisterGate("Scathach 4", "_moontunnel_scathach_04", 0xA00FAE00030005);
+		RegisterGate("Scathach 1", "_moontunnel_scathach_01", 0xA00FAE000A00B5);
+		RegisterGate("Scathach 2", "_moontunnel_scathach_02", 0xA00FAE000A00B8);
+		RegisterGate("Scathach 5", "_moontunnel_scathach_05", 0xA00FAE000C004B);
 
-		// TODO: G1?
+		// TODO: G1, G7, G9, G10
 
 		RegisterTimetable("G2",
 			"_moontunnel_fiodh_dungeon",
@@ -118,13 +118,11 @@ public class MoongateScript : GeneralScript
 			"_moontunnel_ciar_dungeon"
 		);
 
-		// TODO: G6, G19, G10
-
 		RegisterTimetable("G11",
 			"_moontunnel_tara_west",
 			"_moontunnel_ceoisland",
 			"_moontunnel_tirchonaill",
-			"_moontunnel_tailteann_druid",
+			"_moontunnel_tailteann_w",
 			"_moontunnel_emainmacha",
 			"_moontunnel_tara_west",
 			"_moontunnel_dunbarton",
@@ -133,7 +131,7 @@ public class MoongateScript : GeneralScript
 			"_moontunnel_emainmacha",
 			"_moontunnel_tara_west",
 			"_moontunnel_tirchonaill",
-			"_moontunnel_tailteann_druid",
+			"_moontunnel_tailteann_w",
 			"_moontunnel_ceoisland",
 			"_moontunnel_emainmacha",
 			"_moontunnel_tara_west",
@@ -141,7 +139,7 @@ public class MoongateScript : GeneralScript
 			"_moontunnel_dunbarton",
 			"_moontunnel_ceann_harbor",
 			"_moontunnel_tara_west",
-			"_moontunnel_tailteann_druid",
+			"_moontunnel_tailteann_w",
 			"_moontunnel_tirchonaill",
 			"_moontunnel_dunbarton",
 			"_moontunnel_bangor"
@@ -163,9 +161,10 @@ public class MoongateScript : GeneralScript
 	private Dictionary<long, MoonGate> gates = new Dictionary<long, MoonGate>();
 	private Dictionary<string, MoonGate> gatesStr = new Dictionary<string, MoonGate>();
 	private Dictionary<string, string[]> tables = new Dictionary<string, string[]>();
-	private string currentGate, nextGate;
+	private string currentGateKeyword, nextGateKeyword;
+	private MoonGate currentGate;
 
-	private void RegisterGate(string keyword, long entityId)
+	private void RegisterGate(string name, string keyword, long entityId)
 	{
 		// Get prop
 		// The moon gate props are client props, they are added to the world
@@ -185,23 +184,11 @@ public class MoongateScript : GeneralScript
 			return;
 		}
 
-		//prop.State = ErinnTime.Now.IsNight || AlwaysOpen ? "open" : "closed";
-
-		// Adds destination name to prop title and a confirmation request
-		// when clicking the prop. With MoonTunnel enabled, this depends on
-		// whether you're a pet or a character, Aura doesn't support that atm.
-		//if (IsEnabled("MoonTunnel"))
-		//{
-		//	prop.Xml.SetAttributeValue("target", currentGate);
-		//	prop.Xml.SetAttributeValue("nopick", 0); // 1 disables clicking the prop
-		//	prop.Extensions.Add(new ConfirmationPropExtension("_devent_ask_warp", string.Format(L("Do you wish to travel to the {0} Moon Gate?"), "Tara"), ""));
-		//}
-
 		// Open map window when prop is clicked
 		SetPropBehavior(entityId, MoonGateBehavior);
 
 		// Save gate
-		var gate = new MoonGate(keyword, keywordData.Id, prop);
+		var gate = new MoonGate(name, keyword, keywordData.Id, prop);
 		gates.Add(entityId, gate);
 		gatesStr.Add(keyword, gate);
 	}
@@ -233,7 +220,7 @@ public class MoongateScript : GeneralScript
 			return;
 		}
 
-		if (IsEnabled("MoonTunnel"))
+		if (IsEnabled("MoonTunnel") && creature.IsCharacter)
 		{
 			// Add keyword if creature doesn't have it yet, this "marks" the
 			// moon gate as potential target in the new system, you can only
@@ -250,7 +237,7 @@ public class MoongateScript : GeneralScript
 		else
 		{
 			var origin = gate.Keyword;
-			var destination = currentGate;
+			var destination = currentGateKeyword;
 
 			// Do you wish to travel to the [...] Moon Gate?
 
@@ -308,7 +295,7 @@ public class MoongateScript : GeneralScript
 	{
 		var creature = client.GetCreatureSafe(packet.Id);
 
-		SendMoonGateInfoRequestR(creature, currentGate, nextGate);
+		SendMoonGateInfoRequestR(creature, currentGateKeyword, nextGateKeyword);
 	}
 
 	private bool UseMoonGate(Creature creature, string origin, string destination)
@@ -398,37 +385,57 @@ public class MoongateScript : GeneralScript
 		creature.Client.Send(packet);
 	}
 
-	private void UpdateCurrentGate()
+	private void UpdateCurrentGates()
 	{
 		var table = GetTable();
 		var cycles = (int)((DateTime.Now - ErinnTime.BeginOfTime).TotalSeconds / 2160);
 
-		currentGate = table[cycles % table.Length];
-		nextGate = table[(cycles + 1) % table.Length];
+		currentGateKeyword = table[cycles % table.Length];
+		nextGateKeyword = table[(cycles + 1) % table.Length];
 
-		SendMoonGateInfoRequestR(currentGate, nextGate);
+		if (!gatesStr.TryGetValue(currentGateKeyword, out currentGate))
+			Log.Error("MoonGateScript.UpdateCurrentGates: Gate '{0}' not found.", currentGateKeyword);
+
+		SendMoonGateInfoRequestR(currentGateKeyword, nextGateKeyword);
 	}
 
 	[On("ErinnDaytimeTick")]
 	public void OnErinnDaytimeTick(ErinnTime now)
 	{
+		if (now.IsDusk || currentGateKeyword == null)
+			UpdateCurrentGates();
+
 		var state = now.IsNight || AlwaysOpen ? "open" : "closed";
 
 		foreach (var gate in gates.Values)
-			gate.Prop.SetState(state);
+		{
+			gate.Prop.State = state;
 
-		if (now.IsDawn || currentGate == null)
-			UpdateCurrentGate();
+			// Adds destination name to prop title and a confirmation request
+			// when clicking the prop. With MoonTunnel enabled, this depends on
+			// whether you're a pet or a character, Aura doesn't support that atm.
+			if (!IsEnabled("MoonTunnel"))
+			{
+				gate.Prop.Xml.SetAttributeValue("target", currentGateKeyword);
+				gate.Prop.Xml.SetAttributeValue("nopick", 0); // 1 disables clicking the prop
+				gate.Prop.RemoveAllExtensions();
+				gate.Prop.AddExtension(new ConfirmationPropExtension("_devent_ask_warp", string.Format(L("Do you wish to travel to the {0} Moon Gate?"), currentGate.Name), ""));
+			}
+
+			Send.PropUpdate(gate.Prop);
+		}
 	}
 
 	private class MoonGate
 	{
+		public string Name { get; set; }
 		public string Keyword { get; set; }
 		public int KeywordId { get; set; }
 		public Prop Prop { get; set; }
 
-		public MoonGate(string keyword, int keywordId, Prop prop)
+		public MoonGate(string name, string keyword, int keywordId, Prop prop)
 		{
+			this.Name = name;
 			this.Keyword = keyword;
 			this.KeywordId = keywordId;
 			this.Prop = prop;
