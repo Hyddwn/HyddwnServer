09d65f2ac33b640ec44fb81f53dafc1c62d3f37b
exec <exec@mabinoger.de>
2015-10-03 09:21:39 +0200

    Added timetable functionality to moon gates

--- system/scripts/others/moongates.cs
+++ system/scripts/others/moongates.cs
@@ -13,7 +13,7 @@ public class MoongateScript : GeneralScript
 	// Setup ----------------------------------------------------------------
 
 	// Moongates are always open
-	private const bool AlwaysOpen = false;
+	private const bool AlwaysOpen = true;
 
 	// Anybody can use any moongate without activating it first
 	private const bool FreeRoaming = false;
@@ -69,18 +69,101 @@ public class MoongateScript : GeneralScript
 		RegisterGate("_moontunnel_scathach_02", 0xA00FAE000A00B8);
 		RegisterGate("_moontunnel_scathach_05", 0xA00FAE000C004B);
 
+		// TODO: G1?
+
+		RegisterTimetable("G2",
+			"_moontunnel_fiodh_dungeon",
+			"_moontunnel_math_dungeon",
+			"_moontunnel_rabbie_dungeon",
+			"_moontunnel_bangor",
+			"_moontunnel_tirchonaill",
+			"_moontunnel_dunbarton",
+			"_moontunnel_tirchonaill",
+			"_moontunnel_dunbarton",
+			"_moontunnel_tirchonaill",
+			"_moontunnel_ciar_dungeon",
+			"_moontunnel_peaca_dungeon",
+			"_moontunnel_bangor",
+			"_moontunnel_dragonruin",
+			"_moontunnel_dunbarton",
+			"_moontunnel_alby_dungeon",
+			"_moontunnel_loggingcamp",
+			"_moontunnel_bangor"
+		);
+
+		RegisterTimetable("G2Emain",
+			"_moontunnel_coill_dungeon",
+			"_moontunnel_alby_dungeon",
+			"_moontunnel_ceoisland",
+			"_moontunnel_peaca_dungeon",
+			"_moontunnel_dunbarton",
+			"_moontunnel_dragonruin",
+			"_moontunnel_ceoisland",
+			"_moontunnel_ceoisland",
+			"_moontunnel_dunbarton",
+			"_moontunnel_rabbie_dungeon",
+			"_moontunnel_emainmacha",
+			"_moontunnel_emainmacha",
+			"_moontunnel_bangor",
+			"_moontunnel_math_dungeon",
+			"_moontunnel_tirchonaill",
+			"_moontunnel_dunbarton",
+			"_moontunnel_tirchonaill",
+			"_moontunnel_tirchonaill",
+			"_moontunnel_bangor",
+			"_moontunnel_bangor",
+			"_moontunnel_fiodh_dungeon",
+			"_moontunnel_emainmacha",
+			"_moontunnel_loggingcamp",
+			"_moontunnel_ciar_dungeon"
+		);
+
+		// TODO: G6, G19, G10
+
+		RegisterTimetable("G11",
+			"_moontunnel_tara_west",
+			"_moontunnel_ceoisland",
+			"_moontunnel_tirchonaill",
+			"_moontunnel_tailteann_druid",
+			"_moontunnel_emainmacha",
+			"_moontunnel_tara_west",
+			"_moontunnel_dunbarton",
+			"_moontunnel_ceann_harbor",
+			"_moontunnel_bangor",
+			"_moontunnel_emainmacha",
+			"_moontunnel_tara_west",
+			"_moontunnel_tirchonaill",
+			"_moontunnel_tailteann_druid",
+			"_moontunnel_ceoisland",
+			"_moontunnel_emainmacha",
+			"_moontunnel_tara_west",
+			"_moontunnel_bangor",
+			"_moontunnel_dunbarton",
+			"_moontunnel_ceann_harbor",
+			"_moontunnel_tara_west",
+			"_moontunnel_tailteann_druid",
+			"_moontunnel_tirchonaill",
+			"_moontunnel_dunbarton",
+			"_moontunnel_bangor"
+		);
+
 		// Keywords don't exist, do the gates still exist?
 		//RegisterGate("_moontunnel_dugaldaisle", 0xA0001000060014);
 		//RegisterGate("_moontunnel_moonsurface_enterance", 0xA003EB00000001);
 		//RegisterGate("_moontunnel_moonsurface_exit", 0xA003EB00000003);
 
-		AddPacketHandler(Op.MoonGateUse, MoonGateUse);
+		AddPacketHandler(Op.MoonGateUse, HandleMoonGateUse);
+		AddPacketHandler(Op.MoonGateInfoRequest, HandleMoonGateInfoRequest);
+
+		OnErinnDaytimeTick(ErinnTime.Now);
 	}
 
 	// End Setup ------------------------------------------------------------
 
 	private Dictionary<long, MoonGate> gates = new Dictionary<long, MoonGate>();
 	private Dictionary<string, MoonGate> gatesStr = new Dictionary<string, MoonGate>();
+	private Dictionary<string, string[]> tables = new Dictionary<string, string[]>();
+	private string currentGate, nextGate;
 
 	private void RegisterGate(string keyword, long entityId)
 	{
@@ -102,10 +185,20 @@ public class MoongateScript : GeneralScript
 			return;
 		}
 
-		prop.State = ErinnTime.Now.IsNight || AlwaysOpen ? "open" : "closed";
+		//prop.State = ErinnTime.Now.IsNight || AlwaysOpen ? "open" : "closed";
+
+		// Adds destination name to prop title and a confirmation request
+		// when clicking the prop. With MoonTunnel enabled, this depends on
+		// whether you're a pet or a character, Aura doesn't support that atm.
+		//if (IsEnabled("MoonTunnel"))
+		//{
+		//	prop.Xml.SetAttributeValue("target", currentGate);
+		//	prop.Xml.SetAttributeValue("nopick", 0); // 1 disables clicking the prop
+		//	prop.Extensions.Add(new ConfirmationPropExtension("_devent_ask_warp", string.Format(L("Do you wish to travel to the {0} Moon Gate?"), "Tara"), ""));
+		//}
 
 		// Open map window when prop is clicked
-		SetPropBehavior(entityId, OpenMapWindow);
+		SetPropBehavior(entityId, MoonGateBehavior);
 
 		// Save gate
 		var gate = new MoonGate(keyword, keywordData.Id, prop);
@@ -113,7 +206,15 @@ public class MoongateScript : GeneralScript
 		gatesStr.Add(keyword, gate);
 	}
 
-	private void OpenMapWindow(Creature creature, Prop prop)
+	private void RegisterTimetable(string name, params string[] gates)
+	{
+		if (gates.Length < 2)
+			throw new ArgumentException("Timetables require at least two gates.");
+
+		tables[name] = gates;
+	}
+
+	private void MoonGateBehavior(Creature creature, Prop prop)
 	{
 		// Get gate
 		// Sanity check, technically it shouldn't ever fail, because we wouldn't
@@ -125,38 +226,106 @@ public class MoongateScript : GeneralScript
 			return;
 		}
 
-		// Add keyword if creature doesn't have it yet, this "marks" the
-		// moon gate as potential target in the new system, you can only
-		// warp to those you have visited before.
-		if (!creature.Keywords.Has(gate.Keyword))
-			creature.Keywords.Give(gate.Keyword);
+		// Check state
+		if (gate.Prop.State == "closed")
+		{
+			Log.Warning("MoonGateScript.MoonGateBehavior: Creature '{0:X16}' tried to use closed moon gate.", creature.EntityId);
+			return;
+		}
 
-		// Get list of moon gates the creature can use
-		var freeRoaming = (FreeRoaming || creature.Keywords.Has("freemoongate"));
-		var mygates = gates.Values.Where(a => CanUseGate(creature, a));
+		if (IsEnabled("MoonTunnel"))
+		{
+			// Add keyword if creature doesn't have it yet, this "marks" the
+			// moon gate as potential target in the new system, you can only
+			// warp to those you have visited before.
+			if (!creature.Keywords.Has(gate.Keyword))
+				creature.Keywords.Give(gate.Keyword);
 
-		SendMoonGateMap(creature, gate, mygates);
+			// Get list of moon gates the creature can use
+			var freeRoaming = (FreeRoaming || creature.Keywords.Has("freemoongate"));
+			var mygates = gates.Values.Where(a => CanWarpTo(creature, a));
+
+			SendMoonGateMap(creature, gate, mygates);
+		}
+		else
+		{
+			var origin = gate.Keyword;
+			var destination = currentGate;
+
+			// Do you wish to travel to the [...] Moon Gate?
+
+			UseMoonGate(creature, origin, destination);
+		}
 	}
 
-	private bool CanUseGate(Creature creature, MoonGate gate)
+	private string[] GetTable()
 	{
+		string[] result;
+
+		// G11
+		if (IsEnabled("G11") && tables.TryGetValue("G11", out result))
+			return result;
+
+		// G1
+
+		// Table changes once Emain is open (G2)
+		var sealBroken = (GlobalVars.Perm["SealStoneId_sealstone_osnasail"] != null || GlobalVars.Perm["SealStoneId_sealstone_south_emainmacha"] != null);
+
+		if (tables.TryGetValue("G2Emain", out result) && sealBroken)
+			return result;
+
+		if (tables.TryGetValue("G2", out result))
+			return result;
+
+		// Fallback
+		Log.Warning("MoonGateScript.GetTable: No suitable timetable found.");
+
+		return new string[] { "_moontunnel_tirchonaill", "_moontunnel_dunbarton" };
+	}
+
+	private bool CanWarpTo(Creature creature, MoonGate gate)
+	{
+		if (!IsEnabled("MoonTunnel"))
+			return true;
+
 		var freeRoaming = (FreeRoaming || creature.Keywords.Has("freemoongate"));
 		return (freeRoaming || creature.Keywords.Has(gate.Keyword));
 	}
 
-	private void MoonGateUse(ChannelClient client, Packet packet)
+	private void HandleMoonGateUse(ChannelClient client, Packet packet)
 	{
 		var origin = packet.GetString();
 		var destination = packet.GetString();
 
 		var creature = client.GetCreatureSafe(packet.Id);
 
+		var success = UseMoonGate(creature, origin, destination);
+
+		SendMoonGateUseR(creature, success);
+	}
+
+	private void HandleMoonGateInfoRequest(ChannelClient client, Packet packet)
+	{
+		var creature = client.GetCreatureSafe(packet.Id);
+
+		SendMoonGateInfoRequestR(creature, currentGate, nextGate);
+	}
+
+	private bool UseMoonGate(Creature creature, string origin, string destination)
+	{
+		// Check locations
+		if (origin == destination)
+		{
+			Send.Notice(creature, Localization.Get("You cannot teleport using the same Moon Gate."));
+			return false;
+		}
+
 		// Check gates
 		MoonGate originGate, destinationGate;
 		if (!gatesStr.TryGetValue(origin, out originGate) || !gatesStr.TryGetValue(destination, out destinationGate))
 		{
 			Send.Notice(creature, Localization.Get("This moon gate is currently not operable. Please report."));
-			return;
+			return false;
 		}
 
 		// Check origin gate
@@ -164,29 +333,27 @@ public class MoongateScript : GeneralScript
 		{
 			// Don't log, someone could be waiting with the map open and
 			// select a destination too late.
-			SendMoonGateUseR(creature, false);
-			return;
+			return false;
 		}
 
 		// Check range to origin gate
 		if (creature.RegionId != originGate.Prop.Info.Region || !creature.GetPosition().InRange(originGate.Prop.GetPosition(), 1000))
 		{
 			// Could happen due to desync? The range at least.
-			SendMoonGateUseR(creature, false);
-			return;
+			Send.Notice(creature, L("You're too far away."));
+			return false;
 		}
 
 		// Check if char has target
-		if (!CanUseGate(creature, destinationGate))
+		if (!CanWarpTo(creature, destinationGate))
 		{
 			Log.Warning("MoongateScript.MoonGateUse: Creature '{0:X16}' tried to warp to moon gate that he can't use.", creature.EntityId);
-			SendMoonGateUseR(creature, false);
-			return;
+			return false;
 		}
 
 		creature.Warp(destinationGate.Prop.GetLocation());
 
-		SendMoonGateUseR(creature, true);
+		return true;
 	}
 
 	private void SendMoonGateMap(Creature creature, MoonGate fromGate, IEnumerable<MoonGate> gates)
@@ -215,6 +382,33 @@ public class MoongateScript : GeneralScript
 		creature.Client.Send(packet);
 	}
 
+	private void SendMoonGateInfoRequestR(string current, string next)
+	{
+		var packet = new Packet(Op.MoonGateInfoRequestR, MabiId.Channel);
+		packet.PutString(current);
+		packet.PutString(next);
+		ChannelServer.Instance.World.Broadcast(packet);
+	}
+
+	private void SendMoonGateInfoRequestR(Creature creature, string current, string next)
+	{
+		var packet = new Packet(Op.MoonGateInfoRequestR, MabiId.Channel);
+		packet.PutString(current);
+		packet.PutString(next);
+		creature.Client.Send(packet);
+	}
+
+	private void UpdateCurrentGate()
+	{
+		var table = GetTable();
+		var cycles = (int)((DateTime.Now - ErinnTime.BeginOfTime).TotalSeconds / 2160);
+
+		currentGate = table[cycles % table.Length];
+		nextGate = table[(cycles + 1) % table.Length];
+
+		SendMoonGateInfoRequestR(currentGate, nextGate);
+	}
+
 	[On("ErinnDaytimeTick")]
 	public void OnErinnDaytimeTick(ErinnTime now)
 	{
@@ -222,6 +416,9 @@ public class MoongateScript : GeneralScript
 
 		foreach (var gate in gates.Values)
 			gate.Prop.SetState(state);
+
+		if (now.IsDawn || currentGate == null)
+			UpdateCurrentGate();
 	}
 
 	private class MoonGate
