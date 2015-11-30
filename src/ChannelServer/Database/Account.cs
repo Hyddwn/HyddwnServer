c714d44fe437a6c53883e2e5afcbf74118c4303f
exec <exec@mabinoger.de>
2015-11-30 22:11:03 +0100

    Improved comments in Account

--- src/ChannelServer/Database/Account.cs
+++ src/ChannelServer/Database/Account.cs
@@ -14,23 +14,56 @@ namespace Aura.Channel.Database
 {
 	public class Account
 	{
-		public string Id { get; set; }
-		public long SessionKey { get; set; }
-
-		public int Authority { get; set; }
-
-		public DateTime LastLogin { get; set; }
-
-		public string BanReason { get; set; }
-		public DateTime BanExpiration { get; set; }
-
-		public List<Character> Characters { get; set; }
-		public List<Pet> Pets { get; set; }
-
-		public ScriptVariables Vars { get; protected set; }
-
 		private int _autobanScore;
 		private int _autobanCount;
+
+		/// <summary>
+		/// The account name, which acts as id.
+		/// </summary>
+		public string Id { get; set; }
+
+		/// <summary>
+		/// The current session key.
+		/// </summary>
+		public long SessionKey { get; set; }
+
+		/// <summary>
+		/// The account's authority level, mainly used for GM commands.
+		/// </summary>
+		public int Authority { get; set; }
+
+		/// <summary>
+		/// The time of the last login.
+		/// </summary>
+		public DateTime LastLogin { get; set; }
+
+		/// <summary>
+		/// The reason the account was banned.
+		/// </summary>
+		public string BanReason { get; set; }
+
+		/// <summary>
+		/// The time at which the ban is lifted.
+		/// </summary>
+		public DateTime BanExpiration { get; set; }
+
+		/// <summary>
+		/// List of characters on the account.
+		/// </summary>
+		public List<Character> Characters { get; set; }
+
+		/// <summary>
+		/// List of pets/partners on the account.
+		/// </summary>
+		public List<Pet> Pets { get; set; }
+
+		/// <summary>
+		/// Account specific variables.
+		/// </summary>
+		/// <remarks>
+		/// Permanent variables are saved across relogs.
+		/// </remarks>
+		public ScriptVariables Vars { get; protected set; }
 
 		/// <summary>
 		/// Account wide bank.
@@ -81,6 +114,9 @@ namespace Aura.Channel.Database
 		/// </summary>
 		public DateTime LastAutobanReduction { get; internal set; }
 
+		/// <summary>
+		/// Creates new account instance.
+		/// </summary>
 		public Account()
 		{
 			this.Characters = new List<Character>();
@@ -91,6 +127,12 @@ namespace Aura.Channel.Database
 			this.LastLogin = DateTime.Now;
 		}
 
+		/// <summary>
+		/// Returns character or pet with the given entity id,
+		/// or null, if entity wasn't found.
+		/// </summary>
+		/// <param name="entityId"></param>
+		/// <returns></returns>
 		public PlayerCreature GetCharacterOrPet(long entityId)
 		{
 			PlayerCreature result = this.Characters.FirstOrDefault(a => a.EntityId == entityId);
@@ -99,6 +141,13 @@ namespace Aura.Channel.Database
 			return result;
 		}
 
+		/// <summary>
+		/// Returns character or pet with the given entity id,
+		/// or throws, if entity wasn't found.
+		/// </summary>
+		/// <param name="entityId"></param>
+		/// <returns></returns>
+		/// <exception cref="SevereViolation">When entity doesn't exist on account.</exception>
 		public PlayerCreature GetCharacterOrPetSafe(long entityId)
 		{
 			var r = this.GetCharacterOrPet(entityId);
@@ -108,11 +157,24 @@ namespace Aura.Channel.Database
 			return r;
 		}
 
+		/// <summary>
+		/// Returns pet with the given entity id,
+		/// or null, if entity wasn't found.
+		/// </summary>
+		/// <param name="entityId"></param>
+		/// <returns></returns>
 		public Pet GetPet(long entityId)
 		{
 			return this.Pets.FirstOrDefault(a => a.EntityId == entityId);
 		}
 
+		/// <summary>
+		/// Returns pet with the given entity id,
+		/// or throws, if entity wasn't found.
+		/// </summary>
+		/// <param name="entityId"></param>
+		/// <returns></returns>
+		/// <exception cref="SevereViolation">When entity doesn't exist on account.</exception>
 		public Pet GetPetSafe(long entityId)
 		{
 			var r = this.GetPet(entityId);
