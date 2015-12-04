15dd2fb9ab0e58116b97a8fbac0b91c4a0c425de
exec <exec@mabinoger.de>
2015-12-04 15:44:22 +0100

    Added Month enum

---
+++ src/Mabi/Const/Month.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+namespace Aura.Mabi.Const
+{
+	public enum Month : byte
+	{
+		Imbolic,
+		AlbanEiler,
+		Baltane,
+		AlbanHeruin,
+		Lughnasadh,
+		AlbanElved,
+		Samhain,
+	}
+}
