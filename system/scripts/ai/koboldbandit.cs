cd08d1b82d702a01831ca3010338569324d806e1
exec <exec@mabinoger.de>
2015-12-26 12:45:27 +0100

    Fixed walk/run mistakes in Kobold AI

--- system/scripts/ai/koboldbandit.cs
+++ system/scripts/ai/koboldbandit.cs
@@ -164,27 +164,13 @@ public class KoboldBanditAi : AiScript
 		{
 			if (Random() < 80)
 				Do(Say(koboldBanditAlert));
-			if (Random() < 55)
-			{
-				Do(Circle(500, 1000, 4000, true));
-			}
-			else
-			{
-				Do(Circle(500, 1000, 4000, false));
-			}
+			Do(Circle(500, 1000, 4000, false));
 		}
 		else
 		{
 			if (Random() < 50)
 				Do(Say(koboldBanditAlert));
-			if (Random() < 50)
-			{
-				Do(Circle(500, 1000, 1000, true, false));
-			}
-			else
-			{
-				Do(Circle(500, 1000, 1000, false, false));
-			}
+			Do(Circle(500, 1000, 1000, false));
 		}
 	}
 
