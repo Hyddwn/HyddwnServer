d7593906b67e29a176622895136727de703853fa
exec <exec@mabinoger.de>
2015-12-28 11:14:51 +0100

    Added Ceo spawns

--- system/scripts/regions/ceo.cs
+++ system/scripts/regions/ceo.cs
@@ -15,12 +15,18 @@ public class CeoRegionScript : RegionScript
 	public override void LoadWarps()
 	{
 		// Ceo Cellar
-		SetPropBehavior(0x00A0003800020069, PropWarp(56,18800,16600, 68,5600,4284));
-		SetPropBehavior(0x00A0004400000001, PropWarp(68,5616,3885, 56,18799,15876));
+		SetPropBehavior(0x00A0003800020069, PropWarp(56, 18800, 16600, 68, 5600, 4284));
+		SetPropBehavior(0x00A0004400000001, PropWarp(68, 5616, 3885, 56, 18799, 15876));
 	}
-	
+
 	public override void LoadSpawns()
 	{
-		// ...
+		// Center
+		CreateSpawner(race: 130006, amount: 50, region: 56, delay: 120, delayMin: 10, delayMax: 20, coordinates: A(20463, 9178, 20463, 17283, 28986, 17283, 28986, 9178)); // Golem
+
+		// East
+		CreateSpawner(race: 80201, amount: 1, region: 56, coordinates: A(29159, 7224, 29159, 9403, 31423, 9403, 31423, 7224)); // Lightning Sprite
+		CreateSpawner(race: 80202, amount: 1, region: 56, coordinates: A(29159, 7224, 29159, 9403, 31423, 9403, 31423, 7224)); // Fire Sprite
+		CreateSpawner(race: 80203, amount: 1, region: 56, coordinates: A(29159, 7224, 29159, 9403, 31423, 9403, 31423, 7224)); // Ice Sprite
 	}
 }
