f54434b7b9d7c1f0d89a9b1c45ede985adc9bdf6
exec <exec@mabinoger.de>
2015-12-28 22:06:16 +0100

    Added Cow AI

---
+++ system/scripts/ai/cow.cs
@@ -0,0 +1,28 @@
+//--- Aura Script -----------------------------------------------------------
+// Cow AI
+//--- Description -----------------------------------------------------------
+// AI for cows.
+//---------------------------------------------------------------------------
+
+[AiScript("cow")]
+public class CowAi : AiScript
+{
+	public CowAi()
+	{
+		SetAggroRadius(2000);
+
+		//HatesAttacking("/cow/");
+	}
+
+	protected override IEnumerable Idle()
+	{
+		Do(Wander());
+		Do(Wait(7000, 20000));
+	}
+
+	protected override IEnumerable Aggro()
+	{
+		Do(Attack(3));
+		Do(Wait(3000));
+	}
+}
