689b3640982744ae5950250f7e2f37aa2967b905
exec <exec@mabinoger.de>
2016-01-04 20:00:33 +0100

    Added Albey Normal dungeon

---
+++ system/scripts/dungeons/tirnanog_dungeon.cs
@@ -0,0 +1,58 @@
+//--- Aura Script -----------------------------------------------------------
+// Albey Dungeon
+//--- Description -----------------------------------------------------------
+// Albey router and script for Albey Normal.
+//---------------------------------------------------------------------------
+
+[DungeonScript("tirnanog_dungeon")]
+public class AlbeyDungeonScript : DungeonScript
+{
+	public override bool Route(Creature creature, Item item, ref string dungeonName)
+	{
+		// Gem dungeons...
+
+		// tirnanog_dungeon
+		return true;
+	}
+
+	public override void OnBoss(Dungeon dungeon)
+	{
+		dungeon.AddBoss(160002, 6); // Light Gargolye
+
+		dungeon.PlayCutscene("bossroom_LightGargoyle");
+	}
+
+	public override void OnCleared(Dungeon dungeon)
+	{
+		var rnd = RandomProvider.Get();
+
+		for (int i = 0; i < dungeon.Party.Count; ++i)
+		{
+			var member = dungeon.Party[i];
+			var treasureChest = new TreasureChest();
+
+			treasureChest.AddGold(rnd.Next(9000, 16000)); // Gold
+			treasureChest.Add(GetRandomTreasureItem(rnd)); // Random item
+
+			dungeon.AddChest(treasureChest);
+
+			member.GiveItemWithEffect(Item.CreateKey(70028, "chest"));
+		}
+	}
+
+	List<DropData> drops;
+	public Item GetRandomTreasureItem(Random rnd)
+	{
+		if (drops == null)
+		{
+			drops = new List<DropData>();
+			drops.Add(new DropData(itemId: 62004, chance: 20, amountMin: 1, amountMax: 2)); // Magic Powder
+			drops.Add(new DropData(itemId: 51102, chance: 20, amountMin: 1, amountMax: 2)); // Mana Herb
+			drops.Add(new DropData(itemId: 51003, chance: 20, amountMin: 1, amountMax: 2)); // HP 50 Potion
+			drops.Add(new DropData(itemId: 51008, chance: 20, amountMin: 1, amountMax: 2)); // MP 50 Potion
+			drops.Add(new DropData(itemId: 51013, chance: 20, amountMin: 1, amountMax: 2)); // Stamina 50 Potion
+		}
+
+		return Item.GetRandomDrop(rnd, drops);
+	}
+}
