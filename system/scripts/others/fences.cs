6123e586d3965c836c4b56baecf6c1c913966509
exec <exec@mabinoger.de>
2016-01-17 17:09:30 +0100

    Added scenery preventing access to Cobh

--- system/scripts/others/fences.cs
+++ system/scripts/others/fences.cs
@@ -24,5 +24,15 @@ public class FeatureFencesScript : GeneralScript
 		// Dugald Aisle -> Dugald Residential Area
 		var prop = ChannelServer.Instance.World.GetRegion(16).GetProp(a => a.Info.Id == 25219); // toggleable fence
 		if (prop != null) prop.SetState(IsEnabled("Housing") ? "open" : "close");
+
+		// Dunbarton -> Port Cobh
+		if (!IsEnabled("G14S4"))
+		{
+			for (int i = 0; i < 5; ++i)
+			{
+				prop = SpawnProp(15035, 14, 62400, 37300 + i * 800, 3.14159f);
+				prop.Info.Color1 = 0xFFFFFF;
+			}
+		}
 	}
 }
