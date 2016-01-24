dcf972a99a7b1c755c710048260b56149faba16b
exec <exec@mabinoger.de>
2016-01-24 18:22:50 +0100

    Added dummy handler for trading
    Related to #76.

---
+++ src/ChannelServer/Network/Sending/Send.Trading.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using Aura.Channel.Network.Sending.Helpers;
+using Aura.Channel.World;
+using Aura.Channel.World.Entities;
+using Aura.Channel.World.Entities.Props;
+using Aura.Mabi.Network;
+using Aura.Shared.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aura.Channel.Network.Sending
+{
+	public static partial class Send
+	{
+		/// <summary>
+		/// Sends TradeStartR to creature's client.
+		/// </summary>
+		/// <param name="creature"></param>
+		/// <param name="success"></param>
+		public static void TradeStartR(Creature creature, bool success)
+		{
+			var packet = new Packet(Op.TradeStartR, creature.EntityId);
+			packet.PutByte(success);
+			creature.Client.Send(packet);
+		}
+	}
+}
