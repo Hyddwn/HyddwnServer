f0625d73a4011d20bc833517d2f84a3b63a0f8dd
exec <exec@mabinoger.de>
2016-02-12 10:17:40 +0100

    Moved number_format option from web to localization

--- src/Shared/Util/Configuration/Files/Localization.cs
+++ src/Shared/Util/Configuration/Files/Localization.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Aura development team - Licensed under GNU GPL
 // For more information, see license file in the main folder
 
+using System.Globalization;
 namespace Aura.Shared.Util.Configuration.Files
 {
 	/// <summary>
@@ -9,12 +10,14 @@ namespace Aura.Shared.Util.Configuration.Files
 	public class LocalizationConfFile : ConfFile
 	{
 		public string Language { get; protected set; }
+		public string Culture { get; protected set; }
 
 		public void Load()
 		{
 			this.Require("system/conf/localization.conf");
 
-			this.Language = this.GetString("language", "us");
+			this.Language = this.GetString("language", "en-US");
+			this.Culture = this.GetString("culture", "en-US");
 		}
 	}
 }
