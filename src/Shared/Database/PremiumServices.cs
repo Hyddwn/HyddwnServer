447405a08a652a74e76a370acdf8e132ea4a64ef
exec <exec@mabinoger.de>
2016-02-26 13:44:29 +0100

    Added premium checks for bank management

--- src/Shared/Database/PremiumServices.cs
+++ src/Shared/Database/PremiumServices.cs
@@ -78,6 +78,12 @@ namespace Aura.Shared.Database
 		public bool CanUseBags { get { return (this.HasInventoryPlusService || this.HasPremiumService || this.HasVipService); } }
 
 		/// <summary>
+		/// Returns true if account is allowed to use all bank tabs,
+		/// not only its own.
+		/// </summary>
+		public bool CanUseAllBankTabs { get { return (this.HasInventoryPlusService || this.HasPremiumService || this.HasVipService); } }
+
+		/// <summary>
 		/// Enables services for free, based on the conf's settings.
 		/// </summary>
 		/// <param name="premiumConfFile"></param>
