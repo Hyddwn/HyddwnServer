3c785f4b60e75d6b7a52f51de957c7c617337549
exec <exec@mabinoger.de>
2016-05-30 09:33:29 +0200

    Fixed destroying the Sword of Elsinore
    Kinda important to get rid of it, now that you can't drop it anymore^^

--- src/ChannelServer/Network/Handlers/Items.cs
+++ src/ChannelServer/Network/Handlers/Items.cs
@@ -316,7 +316,11 @@ namespace Aura.Channel.Network.Handlers
 			var item = creature.Inventory.GetItemSafe(itemEntityId);
 
 			// Check if item is destroyable
-			if (!item.HasTag("/destroyable/"))
+			// The check for the Sword of Elsinore is a terrible hack,
+			// but in my defense, it was devCAT's idea. Instead of adding the
+			// destroyable tag to the item, the client checks for the
+			// hamlets_sword tag >_>
+			if (!item.HasTag("/destroyable/|/hamlets_sword/"))
 			{
 				Log.Warning("ItemDestroy: Creature '{0:X16}' tried to destroy a non-destroyable item.", creature.EntityId);
 				Send.ItemDestroyR(creature, false);
