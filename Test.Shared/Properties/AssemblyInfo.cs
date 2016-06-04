bc651e993845d2dc0277ed57b3624534be8ef558
exec <exec@mabinoger.de>
2016-06-04 12:50:35 +0200

    Added unit test for Math2.MultiplyChecked

---
+++ Test.Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("Test.Shared")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Test.Shared")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("c0e355f9-0e9e-4bea-85ec-4d256d41e83b")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test.Shared/Util/Math2.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using Aura.Shared.Util;
+using Xunit;
+
+namespace Aura.Tests.Shared
+{
+	public class Math2Tests
+	{
+		[Fact]
+		public void MultiplyCheckedInt()
+		{
+			// Positive
+			Assert.Equal(100000, Math2.MultiplyChecked(50000, 2));
+			Assert.Equal(2000000000, Math2.MultiplyChecked(1000000000, 2));
+			Assert.Equal(int.MaxValue, Math2.MultiplyChecked(2000000000, 2));
+
+			// Negative
+			Assert.Equal(-100000, Math2.MultiplyChecked(-50000, 2));
+			Assert.Equal(-2000000000, Math2.MultiplyChecked(-1000000000, 2));
+			Assert.Equal(int.MinValue, Math2.MultiplyChecked(-2000000000, 2));
+		}
+
+		[Fact]
+		public void MultiplyCheckedLong()
+		{
+			// Positive
+			Assert.Equal(100000L, Math2.MultiplyChecked(50000L, 2));
+			Assert.Equal(2000000000L, Math2.MultiplyChecked(1000000000L, 2));
+			Assert.Equal(4000000000L, Math2.MultiplyChecked(2000000000L, 2));
+			Assert.Equal(long.MaxValue, Math2.MultiplyChecked(5000000000000000000, 2));
+
+			// Negative
+			Assert.Equal(-100000L, Math2.MultiplyChecked(-50000, 2));
+			Assert.Equal(-2000000000L, Math2.MultiplyChecked(-1000000000L, 2));
+			Assert.Equal(-4000000000L, Math2.MultiplyChecked(-2000000000L, 2));
+			Assert.Equal(long.MinValue, Math2.MultiplyChecked(-5000000000000000000, 2));
+		}
+	}
+}
