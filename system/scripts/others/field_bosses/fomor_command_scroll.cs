ae189095fdba753eb8d767ee7df70a1faff79448
exec <exec@mabinoger.de>
2016-07-08 18:03:20 +0200

    Moved time span string function to GeneralScript

--- system/scripts/others/field_bosses/fomor_command_scroll.cs
+++ system/scripts/others/field_bosses/fomor_command_scroll.cs
@@ -27,40 +27,7 @@ public class FomorCommandScrollItemScript : ItemScript
 		var bossName = item.MetaData1.GetString("BSGRNM");
 		var locationName = item.MetaData1.GetString("BSGRPS");
 		var announcerName = creature.Name;
-
-		// Calculate days
-		var m1 = (erinnNow.Year * 7 * 40 * 24 * 60) + (erinnNow.Month * 40 * 24 * 60) + (erinnNow.Day * 24 * 60) + (erinnNow.Hour * 60) + erinnNow.Minute;
-		var m2 = (erinnSpawn.Year * 7 * 40 * 24 * 60) + (erinnSpawn.Month * 40 * 24 * 60) + (erinnSpawn.Day * 24 * 60) + (erinnSpawn.Hour * 60) + erinnSpawn.Minute;
-		var days = (m2 - m1) / 60.0 / 24.0;
-
-		if (erinnSpawn.Hour * 100 + erinnSpawn.Minute > erinnNow.Hour * 100 + erinnNow.Minute)
-			days = Math.Floor(days);
-		else
-			days = Math.Ceiling(days);
-
-		// Get days part
-		var time = "";
-		if (erinnSpawn.DateTimeStamp == erinnNow.DateTimeStamp)
-			time = L("Today");
-		else if (erinnSpawn.DateTimeStamp == erinnNow.DateTimeStamp + 1)
-			time = L("Tomorrow");
-		else if (erinnSpawn.DateTimeStamp == erinnNow.DateTimeStamp + 2)
-			time = L("the day after tomorrow");
-		else
-			time = string.Format(LN("{0} day", "{0} days", (int)days), (int)days);
-
-		// Get time of day part
-		var hour = erinnSpawn.Hour;
-		if (hour >= 20)
-			time += L(" night");
-		else if (hour >= 12)
-			time += L(" afternoon");
-		else if (hour >= 6)
-			time += L(" morning");
-		else if (hour >= 0)
-			time += L(" dawn");
-
-		// Send message
+		var time = GetTimeSpanString(erinnNow, erinnSpawn);
 		var msg = string.Format(Localization.Get("{0} has informed that {1} will appear in {2} at {3}."), announcerName, bossName, locationName, time);
 
 		Send.Notice(creature.Region, NoticeType.Top, 20000, msg);
