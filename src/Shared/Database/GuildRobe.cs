cc002d5ff28ef07d245de9670f22f4aa50b6152f
exec <exec@mabinoger.de>
2016-08-25 23:37:02 +0200

    Added guild robe information to database

---
+++ src/Shared/Database/GuildRobe.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+namespace Aura.Shared.Database
+{
+	public class GuildRobe
+	{
+		public byte EmblemMark { get; set; }
+		public byte EmblemOutline { get; set; }
+		public byte Stripes { get; set; }
+		public uint RobeColor { get; set; }
+		public byte BadgeColor { get; set; }
+		public byte EmblemMarkColor { get; set; }
+		public byte EmblemOutlineColor { get; set; }
+		public byte StripesColor { get; set; }
+	}
+}
