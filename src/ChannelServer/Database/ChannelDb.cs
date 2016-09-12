900f21ed0684df8cf907000e273a8869b9e4938f
QewQew <QewQew@users.noreply.github.com>
2016-09-12 00:43:02 -0700

    Removed extra space
    that was inserted by the Github editor.

--- src/ChannelServer/Database/ChannelDb.cs
+++ src/ChannelServer/Database/ChannelDb.cs
@@ -1,4 +1,4 @@
- // Copyright (c) Aura development team - Licensed under GNU GPL
+// Copyright (c) Aura development team - Licensed under GNU GPL
 // For more information, see license file in the main folder
 
 using Aura.Channel.Network;
