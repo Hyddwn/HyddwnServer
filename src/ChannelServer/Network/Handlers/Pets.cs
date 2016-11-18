c8eb35736b2db53ccf03b0b8331469e2cd5d8795
exec <exec@mabinoger.de>
2016-11-18 14:04:55 +0100

    Follow up 004553c64c8f7199cd3efd3f36e2ab9faaab7497
    Another packet that may be sent from a despawned pet. See #18.

--- src/ChannelServer/Network/Handlers/Pets.cs
+++ src/ChannelServer/Network/Handlers/Pets.cs
@@ -132,7 +132,12 @@ namespace Aura.Channel.Network.Handlers
 			var entityId = packet.GetLong();
 			var action = (PetAction)packet.GetByte();
 
-			var pet = client.GetCreatureSafe(packet.Id);
+			// Don't use GetCreatureSafe here, since pets may send
+			// another packet after they have already been despawned,
+			// which kicks their master.
+			var pet = client.GetCreature(packet.Id);
+			if (pet == null)
+				return;
 
 			// Check action
 			if (!Enum.IsDefined(typeof(PetAction), action))
