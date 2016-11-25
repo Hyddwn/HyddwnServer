7e9caa16f59d5afc4cebd6d4de761e5be4635a72
exec <exec@mabinoger.de>
2016-11-25 13:03:50 +0100

    Minor text fixes in test event

--- system/scripts/events/test_event.cs
+++ system/scripts/events/test_event.cs
@@ -3,23 +3,22 @@ public class TestEventScript : GameEventScript
 	public override void Load()
 	{
 		SetId("aura_test_event");
-		SetName(L("Test Event"));
+		SetName(L("Test"));
 	}
 
 	public override void AfterLoad()
 	{
-		ScheduleEvent(Id, DateTime.Parse("2016-11-23 22:23"), DateTime.Parse("2016-11-23 22:24"));
 		ScheduleEvent(Id, DateTime.Parse("2016-11-25 13:00"), DateTime.Parse("2016-11-25 13:05"));
 	}
 
 	protected override void OnStart()
 	{
-		Send.Notice(NoticeType.Middle, "test even started");
+		Send.Notice(NoticeType.Middle, "test event started");
 		AddGlobalBonus(GlobalBonusStat.CombatExp, 2);
 	}
 
 	protected override void OnEnd()
 	{
-		Send.Notice(NoticeType.Middle, "test even stopped");
+		Send.Notice(NoticeType.Middle, "test event stopped");
 	}
 }
