4834a6307e4fd6a6a81ae6a5c28d7a357137ee64
exec <exec@mabinoger.de>
2016-12-02 11:19:22 +0100

    Added ErinnTime.GetNextTime

--- src/Mabi/ErinnTime.cs
+++ src/Mabi/ErinnTime.cs
@@ -200,6 +200,58 @@ namespace Aura.Mabi
 			_months[5] = albanElved;
 			_months[6] = samhain;
 		}
+
+		/// <summary>
+		/// Returns an instance of ErinnTime that's set to the next time the
+		/// given hour and minute is.
+		/// </summary>
+		/// <example>
+		/// If it's 00:00:00 in real-time (00:00 in Erinn-time), then
+		/// GetNextTime(12, 00) would return Erinn-time 12:00, with a
+		/// DateTime of 00:18:00.
+		/// </example>
+		/// <param name="hour"></param>
+		/// <param name="minutes"></param>
+		/// <returns></returns>
+		public static ErinnTime GetNextTime(int hour, int minute)
+		{
+			return GetNextTime(DateTime.Now, hour, minute);
+		}
+
+		/// <summary>
+		/// Returns an instance of ErinnTime that's set to the next time the
+		/// given hour and minute is.
+		/// </summary>
+		/// <example>
+		/// If it's 00:00:00 in real-time (00:00 in Erinn-time), then
+		/// GetNextTime(12, 00) would return Erinn-time 12:00, with a
+		/// DateTime of 00:18:00.
+		/// </example>
+		/// <param name="hour"></param>
+		/// <param name="minutes"></param>
+		/// <returns></returns>
+		public static ErinnTime GetNextTime(DateTime now, int hour, int minute)
+		{
+			if (hour < 0 || hour > 23)
+				throw new ArgumentException("Invalid hour.");
+			if (minute < 0 || minute > 59)
+				throw new ArgumentException("Invalid minute.");
+
+			var nowErinn = new ErinnTime(now);
+			var nowTimeStamp = (nowErinn.Hour * 100) + nowErinn.Minute;
+			var thenTimeStamp = (hour * 100) + minute;
+
+			var diff = thenTimeStamp - nowTimeStamp;
+			var rollover = (diff == 0 || hour < nowErinn.Hour || (hour == nowErinn.Hour && minute < nowErinn.Minute));
+			if (rollover)
+				diff += 2400;
+
+			var hours = (diff / 100);
+			var minutes = (diff - hours * 100);
+			var thenDateTime = now.AddTicks(hours * TicksPerHour).AddTicks(minutes * TicksPerMinute);
+
+			return new ErinnTime(thenDateTime);
+		}
 	}
 
 	public static class ErinnMonth
