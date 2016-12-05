7554e798d0ebbd7167e6cf95aa16726e47ad33a6
Thunder Bro <jimmyortner@cox.net>
2016-12-04 18:54:22 -0700

    Update 71038_poison_goblin.cs

--- system/scripts/quests/scrolls/71038_poison_goblin.cs
+++ system/scripts/quests/scrolls/71038_poison_goblin.cs
@@ -13,6 +13,7 @@ public class PoisonGoblinScrollQuest : QuestScript
 		SetName(L("Collect the Poison Goblin's Fomor Scrolls"));
 		SetDescription(L("The evil Fomors are controlling various creatures in the neighborhood. Retrieve Fomor Scrolls from these animals in order to free them from the reign of these evil spirits. You will be rewarded for collecting [10 Poison Goblin Fomor Scrolls]."));
 		SetType(QuestType.Collect);
+		SetCancelable(true);
 
 		SetIcon(QuestIcon.Collect);
 		if (IsEnabled("QuestViewRenewal"))
