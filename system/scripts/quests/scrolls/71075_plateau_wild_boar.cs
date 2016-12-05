d0796cf51ce644bb6464dc78436c7d9d845feda8
Thunder Bro <jimmyortner@cox.net>
2016-12-04 19:00:29 -0700

    Update 71075_plateau_wild_boar.cs

--- system/scripts/quests/scrolls/71075_plateau_wild_boar.cs
+++ system/scripts/quests/scrolls/71075_plateau_wild_boar.cs
@@ -13,6 +13,7 @@ public class PlateauWildBoarScrollQuest : QuestScript
 		SetName(L("Collect the Plateau Wild Boar's Fomor Scrolls"));
 		SetDescription(L("The evil Fomors are controlling various creatures in the neighborhood. Retrieve Fomor Scrolls from these animals in order to free them from the reign of these evil spirits. You will be rewarded for collecting [10 Plateau Wild Boar Fomor Scrolls]."));
 		SetType(QuestType.Collect);
+		SetCancelable(true);
 
 		SetIcon(QuestIcon.Collect);
 		if (IsEnabled("QuestViewRenewal"))
