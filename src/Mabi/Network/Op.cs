97ae95faa0382ed145fc4c89631a86702ca9dbcd
exec <exec@mabinoger.de>
2017-01-03 13:00:34 +0100

    Updated AmmoRequired op

--- src/Mabi/Network/Op.cs
+++ src/Mabi/Network/Op.cs
@@ -690,7 +690,8 @@ namespace Aura.Mabi.Network
 		public const int DestroyExpiredItemsR = 0xABB6;
 
 		// [200100, NA229 (2016-04-16)] Shifted by +8, from ABBB to ABC3.
-		public const int AmmoRequired = 0xABC3;
+		// [200200, NA242 (2016-12-15)] Shifted by +2, from ABC3 to ABC5.
+		public const int AmmoRequired = 0xABC5;
 
 		// [200100, NA226 (2016-04-14)] Shifted by 4, from AC0A to AC0E.
 		// [200100, NA229 (2016-06-16)] Shifted by +8, from AC0E to AC16.
