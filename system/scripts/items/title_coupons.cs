e5a02169f970044587aeb0bcedef665bd9eae553
Cryptic <crypticvideos@gmail.com>
2017-02-17 19:05:07 -0500

    Added More Coupon Title Functionality
    also added back cosumed due to 43497c5ba6

--- system/scripts/items/title_coupons.cs
+++ system/scripts/items/title_coupons.cs
@@ -1,3 +1,201 @@
+[ItemScript(75688)]
+public class SAOKiritoTitleItemScript : ItemScript
+{
+	public override void OnUse(Creature cr, Item i, string param)
+	{
+		if (!cr.KnowsTitle(10684))
+		{
+			cr.EnableTitle(10684);
+			Send.Notice(cr, L("The title \"SAO Kirito\" has been added!"));
+		}
+		else
+		{
+			Send.Notice(cr, L("You already have this title."));
+			cr.GiveItem(75688);
+		}
+	}
+}
+
+[ItemScript(75689)]
+public class SAOAsunaTitleItemScript : ItemScript
+{
+	public override void OnUse(Creature cr, Item i, string param)
+	{
+		if (!cr.KnowsTitle(10685))
+		{
+			cr.EnableTitle(10685);
+			Send.Notice(cr, L("The title \"SAO Asuna\" has been added!"));
+		}
+		else
+		{
+			Send.Notice(cr, L("You already have this title."));
+			cr.GiveItem(75689);
+		}
+	}
+}
+
+[ItemScript(75690)]
+public class SAOHeathcliffTitleItemScript : ItemScript
+{
+	public override void OnUse(Creature cr, Item i, string param)
+	{
+		if (!cr.KnowsTitle(10686))
+		{
+			cr.EnableTitle(10686);
+			Send.Notice(cr, L("The title \"SAO Heathcliff\" has been added!"));
+		}
+		else
+		{
+			Send.Notice(cr, L("You already have this title."));
+			cr.GiveItem(75690);
+		}
+	}
+}
+
+[ItemScript(75691)]
+public class SAOLisbethTitleItemScript : ItemScript
+{
+	public override void OnUse(Creature cr, Item i, string param)
+	{
+		if (!cr.KnowsTitle(10687))
+		{
+			cr.EnableTitle(10687);
+			Send.Notice(cr, L("The title \"SAO Lisbeth\" has been added!"));
+		}
+		else
+		{
+			Send.Notice(cr, L("You already have this title."));
+			cr.GiveItem(75691);
+		}
+	}
+}
+
+[ItemScript(75692)]
+public class SAONishidaTitleItemScript : ItemScript
+{
+	public override void OnUse(Creature cr, Item i, string param)
+	{
+		if (!cr.KnowsTitle(10688))
+		{
+			cr.EnableTitle(10688);
+			Send.Notice(cr, L("The title \"SAO Nishida\" has been added!"));
+		}
+		else
+		{
+			Send.Notice(cr, L("You already have this title."));
+			cr.GiveItem(75692);
+		}
+	}
+}
+
+[ItemScript(75693)]
+public class SAOPinaTitleItemScript : ItemScript
+{
+	public override void OnUse(Creature cr, Item i, string param)
+	{
+		if (!cr.KnowsTitle(10689))
+		{
+			cr.EnableTitle(10689);
+			Send.Notice(cr, L("The title \"SAO Pina\" has been added!"));
+		}
+		else
+		{
+			Send.Notice(cr, L("You already have this title."));
+			cr.GiveItem(75693);
+		}
+	}
+}
+
+[ItemScript(75694)]
+public class ALOKiritoTitleItemScript : ItemScript
+{
+	public override void OnUse(Creature cr, Item i, string param)
+	{
+		if (!cr.KnowsTitle(10690))
+		{
+			cr.EnableTitle(10690);
+			Send.Notice(cr, L("The title \"ALO Kirito\" has been added!"));
+		}
+		else
+		{
+			Send.Notice(cr, L("You already have this title."));
+			cr.GiveItem(75694);
+		}
+	}
+}
+
+[ItemScript(75695)]
+public class ALOAsunaTitleItemScript : ItemScript
+{
+	public override void OnUse(Creature cr, Item i, string param)
+	{
+		if (!cr.KnowsTitle(10691))
+		{
+			cr.EnableTitle(10691);
+			Send.Notice(cr, L("The title \"ALO Asuna\" has been added!"));
+		}
+		else
+		{
+			Send.Notice(cr, L("You already have this title."));
+			cr.GiveItem(75695);
+		}
+	}
+}
+
+[ItemScript(75696)]
+public class SAOSilicaTitleItemScript : ItemScript
+{
+	public override void OnUse(Creature cr, Item i, string param)
+	{
+		if (!cr.KnowsTitle(10693))
+		{
+			cr.EnableTitle(10693);
+			Send.Notice(cr, L("The title \"SAO Silica\" has been added!"));
+		}
+		else
+		{
+			Send.Notice(cr, L("You already have this title."));
+			cr.GiveItem(75696);
+		}
+	}
+}
+
+[ItemScript(75697)]
+public class SAOKleinTitleItemScript : ItemScript
+{
+	public override void OnUse(Creature cr, Item i, string param)
+	{
+		if (!cr.KnowsTitle(10692))
+		{
+			cr.EnableTitle(10692);
+			Send.Notice(cr, L("The title \"SAO Klein\" has been added!"));
+		}
+		else
+		{
+			Send.Notice(cr, L("You already have this title."));
+			cr.GiveItem(75697);
+		}
+	}
+}
+
+[ItemScript(75698)]
+public class HalloweenPartyTitleItemScript : ItemScript
+{
+	public override void OnUse(Creature cr, Item i, string param)
+	{
+		if (!cr.KnowsTitle(10694))
+		{
+			cr.EnableTitle(10694);
+			Send.Notice(cr, L("The title \"Halloween Party\" has been added!"));
+		}
+		else
+		{
+			Send.Notice(cr, L("You already have this title."));
+			cr.GiveItem(75698);
+		}
+	}
+}
+
 [ItemScript(75707)]
 public class SantaClausTitleItemScript : ItemScript
 {
