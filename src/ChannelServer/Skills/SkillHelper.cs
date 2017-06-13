208bc2096f5d10efde3d0c3883ad6aa3dc6082f2
xeroplz <jtjonson16@gmail.com>
2017-06-13 08:13:02 -0400

    Added extra GetCreaturesInRectangle / GetTargetableCreaturesInSkillArea methods
    - used for other skills that do not need the out parameter, but do need
    the rectangular area

--- src/ChannelServer/Skills/SkillHelper.cs
+++ src/ChannelServer/Skills/SkillHelper.cs
@@ -139,6 +139,25 @@ namespace Aura.Channel.Skills
 
 		/// <summary>
 		/// Gets targetable creatures within a skill's rectangular area (includes collision check).
+		/// </summary>
+		/// <param name="attacker"></param>
+		/// <param name="skillLength"></param>
+		/// <param name="skillWidth"></param>
+		/// <returns></returns>
+		public static List<Creature> GetTargetableCreaturesInSkillArea(Creature attacker, int skillLength, int skillWidth)
+		{
+			if (attacker == null)
+				return null;
+
+			var attackerPos = attacker.GetPosition();
+			var r = Mabi.MabiMath.ByteToRadian(attacker.Direction);
+
+			return attacker.Region.GetCreaturesInRectangle(attackerPos, r, skillLength, skillWidth).Where(x => attacker.CanTarget(x) && !attacker.Region.Collisions.Any(attacker.GetPosition(), x.GetPosition())).ToList();
+
+		}
+
+		/// <summary>
+		/// Gets targetable creatures within a skill's rectangular area (includes collision check).
 		/// Outs an end position on the far end of the rectangle, opposite the attacker position.
 		/// </summary>
 		/// <param name="attacker"></param>
