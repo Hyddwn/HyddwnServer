ee8211cd6226448cf77ad4fc0d370a13b3d6a625
Thunder Bro <jimmyortner@cox.net>
2017-06-23 17:48:44 +0200

    Reduce code redundancy

--- system/scripts/npcs/tir/caitin.cs
+++ system/scripts/npcs/tir/caitin.cs
@@ -450,7 +450,33 @@ public class CaitinShop : NpcShopScript
 {
 	public override void Setup()
 	{
-		Add("Grocery");
+		Add("Grocery", 50001);     // Big Lump of Cheese
+		Add("Grocery", 50002);     // Slice of Cheese
+		Add("Grocery", 50004);     // Bread
+		Add("Grocery", 50005);     // Large Meat
+		Add("Grocery", 50006, 5);  // Sliced Meat x5
+		Add("Grocery", 50111);     // Carrot x1
+		Add("Grocery", 50112);     // Strawberry x1
+		Add("Grocery", 50112, 10); // Strawberry x10
+		Add("Grocery", 50121);     // Butter x1
+		Add("Grocery", 50121, 10); // Butter x10
+		Add("Grocery", 50138);     // Cabbage x1
+		Add("Grocery", 50139);     // Button Mushroom x1
+		Add("Grocery", 50142);     // Onion x1
+		Add("Grocery", 50142, 10); // Onion x10
+		Add("Grocery", 50131);     // Sugar x1
+		Add("Grocery", 50131, 10); // Sugar x10
+		Add("Grocery", 50132);     // Salt x1
+		Add("Grocery", 50132, 10); // Salt x10
+		Add("Grocery", 50156);     // Pepper x1
+		Add("Grocery", 50156, 10); // Pepper x10
+		Add("Grocery", 50145);     // Olive Oil x1
+		Add("Grocery", 50145, 10); // Olive Oil x10
+		Add("Grocery", 50153);     // Deep Fry Batter x1
+		Add("Grocery", 50148);     // Yeast x1
+		Add("Grocery", 50148, 10); // Yeast x10
+		Add("Grocery", 50130);     // Whipped Cream x1
+		Add("Grocery", 50130, 5);  // Whipped Cream x5
 
 		Add("Gift", 52010); // Ramen
 		Add("Gift", 52019); // Heart Cake
@@ -471,8 +497,6 @@ public class CaitinShop : NpcShopScript
 		
 		if (IsEnabled("MasterChef"))
 		{
-			Add("Grocery", 50004);     // Bread
-			Add("Grocery", 50002);     // Slice of Cheese
 			Add("Grocery", 50220);     // Corn Powder
 			Add("Grocery", 50206);     // Chocolate
 			Add("Grocery", 50217);     // Celery
@@ -486,7 +510,6 @@ public class CaitinShop : NpcShopScript
 			Add("Grocery", 50218, 20); // Tomato x20
 			Add("Grocery", 50045);     // Pine Nut
 			Add("Grocery", 50047);     // Camellia Seeds
-			Add("Grocery", 50111);     // Carrot x1
 			Add("Grocery", 50111, 5);  // Carrot x5
 			Add("Grocery", 50111, 10); // Carrot x10
 			Add("Grocery", 50111, 20); // Carrot x20
@@ -502,72 +525,49 @@ public class CaitinShop : NpcShopScript
 			Add("Grocery", 50127, 5);  // Shrimp x5
 			Add("Grocery", 50127, 10); // Shrimp x10
 			Add("Grocery", 50127, 20); // Shrimp x20
-			Add("Grocery", 50131);     // Sugar x1
 			Add("Grocery", 50131, 5);  // Sugar x5
-			Add("Grocery", 50131, 10); // Sugar x10
 			Add("Grocery", 50131, 20); // Sugar x20
-			Add("Grocery", 50132);     // Salt x1
 			Add("Grocery", 50132, 5);  // Salt x5
-			Add("Grocery", 50132, 10); // Salt x10
 			Add("Grocery", 50132, 20); // Salt x20
-			Add("Grocery", 50148);     // Yeast x1
 			Add("Grocery", 50148, 5);  // Yeast x5
-			Add("Grocery", 50148, 10); // Yeast x10
 			Add("Grocery", 50148, 20); // Yeast x20
-			Add("Grocery", 50153);     // Deep Fry Batter x1
 			Add("Grocery", 50153, 5);  // Deep Fry Batter x5
 			Add("Grocery", 50153, 10); // Deep Fry Batter x10
 			Add("Grocery", 50153, 20); // Deep Fry Batter x20
-			Add("Grocery", 50156);     // Pepper x1
 			Add("Grocery", 50156, 5);  // Pepper x5
-			Add("Grocery", 50156, 10); // Pepper x10
 			Add("Grocery", 50156, 20); // Pepper x20
 			Add("Grocery", 50185, 1);  // Curry Powder x1
 			Add("Grocery", 50185, 5);  // Curry Powder x5
 			Add("Grocery", 50185, 10); // Curry Powder x10
 			Add("Grocery", 50185, 20); // Curry Powder x20
 			Add("Grocery", 50046);     // Juniper Berry
-			Add("Grocery", 50112);     // Strawberry x1
 			Add("Grocery", 50112, 5);  // Strawberry x5
-			Add("Grocery", 50112, 10); // Strawberry x10
 			Add("Grocery", 50112, 20); // Strawberry x20
-			Add("Grocery", 50121);     // Butter x1
 			Add("Grocery", 50121, 5);  // Butter x5
-			Add("Grocery", 50121, 10); // Butter x10
 			Add("Grocery", 50121, 20); // Butter x20
-			Add("Grocery", 50142);     // Onion x1
 			Add("Grocery", 50142, 5);  // Onion x5
-			Add("Grocery", 50142, 10); // Onion x10
 			Add("Grocery", 50142, 20); // Onion x20
 			Add("Grocery", 50108);     // Chicken Wings x1
 			Add("Grocery", 50108, 5);  // Chicken Wings x5
 			Add("Grocery", 50108, 10); // Chicken Wings x10
 			Add("Grocery", 50108, 20); // Chicken Wings x20
-			Add("Grocery", 50130);     // Whipped Cream x1
 			Add("Grocery", 50130, 5);  // Whipped Cream x5
-			Add("Grocery", 50130, 10); // Whipped Cream x10
 			Add("Grocery", 50130, 20); // Whipped Cream x20
 			Add("Grocery", 50186);     // Red Pepper Powder x1
 			Add("Grocery", 50186, 5);  // Red Pepper Powder x5
 			Add("Grocery", 50186, 10); // Red Pepper Powder x10
 			Add("Grocery", 50186, 20); // Red Pepper Powder x20
-			Add("Grocery", 50005);     // Large Meat
-			Add("Grocery", 50001);     // Big Lump of Cheese
 			Add("Grocery", 50135);     // Rice x1
 			Add("Grocery", 50135, 5);  // Rice x5
 			Add("Grocery", 50135, 10); // Rice x10
 			Add("Grocery", 50135, 20); // Rice x20
-			Add("Grocery", 50138);     // Cabbage x1
 			Add("Grocery", 50138, 5);  // Cabbage x5
 			Add("Grocery", 50138, 10); // Cabbage x10
 			Add("Grocery", 50138, 20); // Cabbage x20
-			Add("Grocery", 50139);     // Button Mushroom x1
 			Add("Grocery", 50139, 5);  // Button Mushroom x5
 			Add("Grocery", 50139, 10); // Button Mushroom x10
 			Add("Grocery", 50139, 20); // Button Mushroom x20
-			Add("Grocery", 50145);     // Olive Oil x1
 			Add("Grocery", 50145, 5);  // Olive Oil x5
-			Add("Grocery", 50145, 10); // Olive Oil x10
 			Add("Grocery", 50145, 20); // Olive Oil x20
 			Add("Grocery", 50187);     // Lemon x1
 			Add("Grocery", 50187, 5);  // Lemon x5
@@ -603,7 +603,6 @@ public class CaitinShop : NpcShopScript
 			Add("Grocery", 50431, 10); // Ripe Pumpkin x10
 			Add("Grocery", 50431, 20); // Ripe Pumpkin x20
 			Add("Grocery", 50006);     // Sliced Meat x1
-			Add("Grocery", 50006, 5);  // Sliced Meat x5
 			Add("Grocery", 50006, 10); // Sliced Meat x10
 			Add("Grocery", 50006, 20); // Sliced Meat x2
 			Add("Grocery", 50123, "QUAL:4:60;"); // Roasted Bacon
@@ -615,33 +614,6 @@ public class CaitinShop : NpcShopScript
 		}
 		else
 		{
-			Add("Grocery", 50001);     // Big Lump of Cheese
-			Add("Grocery", 50002);     // Slice of Cheese
-			Add("Grocery", 50004);     // Bread
-			Add("Grocery", 50005);     // Large Meat
-			Add("Grocery", 50006, 5);  // Sliced Meat x5
-			Add("Grocery", 50111);     // Carrot x1
-			Add("Grocery", 50112);     // Strawberry x1
-			Add("Grocery", 50112, 10); // Strawberry x10
-			Add("Grocery", 50121);     // Butter x1
-			Add("Grocery", 50121, 10); // Butter x10
-			Add("Grocery", 50138);     // Cabbage x1
-			Add("Grocery", 50139);     // Button Mushroom x1
-			Add("Grocery", 50142);     // Onion x1
-			Add("Grocery", 50142, 10); // Onion x10
-			Add("Grocery", 50131);     // Sugar x1
-			Add("Grocery", 50131, 10); // Sugar x10
-			Add("Grocery", 50132);     // Salt x1
-			Add("Grocery", 50132, 10); // Salt x10
-			Add("Grocery", 50156);     // Pepper x1
-			Add("Grocery", 50156, 10); // Pepper x10
-			Add("Grocery", 50145);     // Olive Oil x1
-			Add("Grocery", 50145, 10); // Olive Oil x10
-			Add("Grocery", 50153);     // Deep Fry Batter x1
-			Add("Grocery", 50148);     // Yeast x1
-			Add("Grocery", 50148, 10); // Yeast x10
-			Add("Grocery", 50130);     // Whipped Cream x1
-			Add("Grocery", 50130, 5);  // Whipped Cream x5
 			Add("Grocery", 50120, "QUAL:4:50;"); // Steamed Rice
 			Add("Grocery", 50134, "QUAL:4:70;"); // Sliced Bread
 			Add("Grocery", 50104, "QUAL:4:60;"); // Egg Salad
